c0790d6fa9b2cc1a44c05417515f649afa53b0d0
Seancheey <adls371@163.com>
2017-03-02 01:53:45 -0500

    Make Unit test a class. Now easy to implement in other files.

--- unit_test_4.py
+++ unit_test_4.py
@@ -2,53 +2,59 @@ from Homework.evalpostfix import eval_postfix
 from Homework.infixtoiter import to_postfix
 from random import randint
 
-operators = ["+", "-", "*", "/", "=", '**']
+
+class ExprUnitTest:
+	operators = []
+	num_range = ()
+	tested_expr = ""
+
+	def __init__(self, operators=["+", "-", "*", "/", '**'], num_range=(-5, 5)):
+		self.operators = operators
+		self.num_range = num_range
+
+	def rand_num(self):
+		return str(randint(1, self.num_range[1])) if randint(1, 2) == 1 else str(randint(self.num_range[0], -1))
+
+	@staticmethod
+	def __rand_space():
+		return " " * randint(0, 1)
+
+	def __rand_operator(self):
+		return self.operators[randint(0, len(self.operators) - 1)]
+
+	def genexpr(self, _len):
+		s = ""
+		curent_len = 0
+		while curent_len < _len - 1:
+			s += (self.rand_num() if randint(0, 5) != 0 else "(" + self.__rand_space() + self.genexpr(
+				2) + self.__rand_space() + ")") + self.__rand_space() + self.__rand_operator() + self.__rand_space()
+			curent_len += 1
+		s += self.rand_num()
+		return s
+
+	def test(self, test_func, correct_output, wrong_output):
+		try:
+			self.tested_expr = self.genexpr(3)
+			cor = eval(self.tested_expr)
+			tes = test_func(self.tested_expr)
+			if cor == tes:
+				print(correct_output)
+			else:
+				print(wrong_output)
+		except ValueError:
+			print('!!get value error')
+		except ZeroDivisionError:
+			print('zero division error')
 
 
-def rand_num():
-	return str(randint(1, 6)) if randint(1, 2) == 1 else str(randint(-6, -1))
-
-
-def rand_space():
-	return " " * randint(0, 1)
-
-
-def rand_operator():
-	return operators[randint(0, len(operators) - 1)]
-
-
-def genexpr(_len):
-	s = ""
-	curent_len = 0
-	while curent_len < _len - 1:
-		s += (rand_num() if randint(0, 5) != 0 else "(" + rand_space() + genexpr(
-			2) + rand_space() + ")") + rand_space() + rand_operator() + rand_space()
-		curent_len += 1
-	s += rand_num()
-	return s
-
-
-def test_eval(e):
-	return eval_postfix(to_postfix(e))
-
-
-def test(_expr):
-	try:
-		if test_eval(_expr) == eval(_expr):
-			print('Correct')
-		else:
-			print(list(to_postfix(_expr)))
-			print('Wrong ', str(_expr), '=', test_eval(expr), 'should be', eval(_expr))
-	except ValueError:
-		print('!!get value error', list(to_postfix(_expr)))
-	except ZeroDivisionError:
-		print('zero division error')
+def test_eval(expr):
+	return eval_postfix(to_postfix(expr))
 
 
 def main():
+	a = ExprUnitTest()
 	for i in range(20):
-		expr = genexpr(3)
-		test(expr)
+		a.test(test_eval, "Correct", "Wrong: "+a.tested_expr)
 
 
 if __name__ == '__main__':
